bf5f3fc10d18bc82894fac30517efc834fb9951e
Manuel Richard Popp <34488159+ManuelPopp@users.noreply.github.com>
2026-02-19 15:01:46 +0100

    Add parallel execution for multiple ECA jobsEdited to publish full script
    Was never run like this, since I first tested it and then didn't want to repeat the test runs. But this combines all runs that were done. When something happens (OOM or other errors), it might need some fiddeling around to avoid re-running everything, i.e., simply delete parts that were already completed.

--- bsh/runECAjobs.sh
+++ bsh/runECAjobs.sh
@@ -1,4 +1,44 @@
 seq 0 24 | xargs -n 1 -P 5 -I {} \
 bash -c 'sleep $(( {} * 8 )); nohup python3 /lud11/poppman/shared/dami/py3/resultsECA.py \
+-rst "top9_C_comp_gridbased.tif" \
+-residx {} > /lud11/poppman/shared/dami/log/nohup{}.out 2> /lud11/poppman/shared/dami/log/nohup{}.err'
+
+seq 0 24 | xargs -n 1 -P 4 -I {} \
+bash -c 'sleep $(( {} * 4 )); nohup python3 /lud11/poppman/shared/dami/py3/resultsECA.py \
+-rst "top9_C_comp_patchbased.tif" \
+-residx {} > /lud11/poppman/shared/dami/log/nohup{}.out 2> /lud11/poppman/shared/dami/log/nohup{}.err'
+
+seq 0 24 | xargs -n 1 -P 4 -I {} \
+bash -c 'sleep $(( {} * 4 )); nohup python3 /lud11/poppman/shared/dami/py3/resultsECA.py \
+-rst "top9_C_con_gridbased.tif" \
+-residx {} > /lud11/poppman/shared/dami/log/nohup{}.out 2> /lud11/poppman/shared/dami/log/nohup{}.err'
+
+seq 0 24 | xargs -n 1 -P 4 -I {} \
+bash -c 'sleep $(( {} * 4 )); nohup python3 /lud11/poppman/shared/dami/py3/resultsECA.py \
+-rst "top9_C_con_patchbased.tif" \
+-residx {} > /lud11/poppman/shared/dami/log/nohup{}.out 2> /lud11/poppman/shared/dami/log/nohup{}.err'
+
+seq 0 24 | xargs -n 1 -P 5 -I {} \
+bash -c 'sleep $(( {} * 8 )); nohup python3 /lud11/poppman/shared/dami/py3/resultsECA.py \
 -rst "top9_R_comp_gridbased.tif" \
+-residx {} > /lud11/poppman/shared/dami/log/nohup{}.out 2> /lud11/poppman/shared/dami/log/nohup{}.err'
+
+seq 0 24 | xargs -n 1 -P 4 -I {} \
+bash -c 'sleep $(( {} * 4 )); nohup python3 /lud11/poppman/shared/dami/py3/resultsECA.py \
+-rst "top9_R_comp_patchbased.tif" \
+-residx {} > /lud11/poppman/shared/dami/log/nohup{}.out 2> /lud11/poppman/shared/dami/log/nohup{}.err'
+
+seq 0 24 | xargs -n 1 -P 4 -I {} \
+bash -c 'sleep $(( {} * 4 )); nohup python3 /lud11/poppman/shared/dami/py3/resultsECA.py \
+-rst "top9_R_con_gridbased.tif" \
+-residx {} > /lud11/poppman/shared/dami/log/nohup{}.out 2> /lud11/poppman/shared/dami/log/nohup{}.err'
+
+seq 0 24 | xargs -n 1 -P 4 -I {} \
+bash -c 'sleep $(( {} * 4 )); nohup python3 /lud11/poppman/shared/dami/py3/resultsECA.py \
+-rst "top9_R_con_patchbased.tif" \
+-residx {} > /lud11/poppman/shared/dami/log/nohup{}.out 2> /lud11/poppman/shared/dami/log/nohup{}.err'
+
+seq 0 24 | xargs -n 1 -P 4 -I {} \
+bash -c 'sleep $(( {} * 4 )); nohup python3 /lud11/poppman/shared/dami/py3/resultsECA.py \
+-rst "baseline_gridbased.tif" \
 -residx {} > /lud11/poppman/shared/dami/log/nohup{}.out 2> /lud11/poppman/shared/dami/log/nohup{}.err'
